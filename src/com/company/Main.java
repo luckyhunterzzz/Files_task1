839ddbb3e77ba28c7d24577d2044104eb0f8d699
luckyhunterzzz <lucky_hunter@list.ru>
2022-04-25 21:12:24 +0500

    second commit add \ to way

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,47 +8,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File mainDirGames = new File ("F:\\Programms\\IDEA-projects\\homeprojects\\files\\files-installed\\Games");
+        String mainDirGames = "F:\\Programms\\IDEA-projects\\homeprojects\\files\\files-installed\\Games\\";
         StringBuilder sb = new StringBuilder();
 
         String[] dirGames = { "src", "res", "savegames", "temp" };
         String[] dirSrc = {"main", "test" };
         String[] dirRes = {"drawables", "vectors", "icons"};
+//        String[] filMain = {"Main.java", "Utils.java" };
+//        String[] filTemp = {"temp.txt" };
 
         for (int i = 0; i < dirGames.length; i++) {
-            File directoryConstructor = new File((mainDirGames + dirGames[i]));
-            if (directoryConstructor.mkdirs()) {
+            File newDir = new File((mainDirGames + dirGames[i]));
+            if (newDir.mkdirs()) {
                 System.out.println("Создана директория " + dirGames[i] + ".");
-                sb.append("Создана директория " + dirGames[i] + ". \n");
+                sb.append("Создана директория " + dirGames[i] + ".\n");
             } else {
                 System.err.println("Не удалось создать директорию " + dirGames[i] + ".");
-                sb.append("Не удалось создать директорию " + dirGames[i] + ". \n");
+                sb.append("Не удалось создать директорию " + dirGames[i] + ".\n");
             }
         }
 
         for (int i = 0; i < dirSrc.length; i++) {
-            File directoryConstructor = new File((mainDirGames + "/src" + dirSrc[i]));
-            if (directoryConstructor.mkdirs()) {
+            File newDir = new File((mainDirGames + "src\\" + dirSrc[i]));
+            if (newDir.mkdirs()) {
                 System.out.println("Создана директория " + dirSrc[i] + ".");
-                sb.append("Создана директория " + dirSrc[i] + ". \n");
+                sb.append("Создана директория " + dirSrc[i] + ".\n");
             } else {
                 System.err.println("Не удалось создать директорию " + dirSrc[i] + ".");
-                sb.append("Не удалось создать директорию " + dirSrc[i] + ". \n");
+                sb.append("Не удалось создать директорию " + dirSrc[i] + ".\n");
             }
         }
 
         for (int i = 0; i < dirRes.length; i++) {
-            File directoryConstructor = new File((mainDirGames + "/res" + dirRes[i]));
-            if (directoryConstructor.mkdirs()) {
+            File newDir = new File((mainDirGames + "res\\" + dirRes[i]));
+            if (newDir.mkdirs()) {
                 System.out.println("Создана директория " + dirRes[i] + ".");
-                sb.append("Создана директория " + dirRes[i] + ". \n");
+                sb.append("Создана директория " + dirRes[i] + ".\n");
             } else {
                 System.err.println("Не удалось создать директорию " + dirRes[i] + ".");
-                sb.append("Не удалось создать директорию " + dirRes[i] + ". \n");
+                sb.append("Не удалось создать директорию " + dirRes[i] + ".\n");
             }
         }
 
-        File dirSrcMainNewFile = new File("F:\\Programms\\IDEA-projects\\homeprojects\\files\\files-installed\\Games\\src\\main");
+        File dirSrcMainNewFile = new File("F:\\Programms\\IDEA-projects\\homeprojects\\files\\files-installed\\Games\\src\\main\\");
 
         File mainJava = new File(dirSrcMainNewFile, "Main.java");
 
@@ -56,13 +58,13 @@ public class Main {
             try {
                 if (mainJava.createNewFile())
                     System.out.println("Создан файл " + mainJava + ".");
-                sb.append("Создан файл " + mainJava + ". \n");
+                sb.append("Создан файл " + mainJava + ".\n");
             } catch (IOException ex) {
                 System.err.println("Не удалось создать файл " + mainJava + ".");
             }
         } else {
             System.out.println("Файл " + mainJava + " уже существует.");
-            sb.append("Файл " + mainJava + " уже существует \n");
+            sb.append("Файл " + mainJava + " уже существует.\n");
         }
 
 
@@ -71,30 +73,30 @@ public class Main {
             try {
                 if (utilsJava.createNewFile())
                     System.out.println("Создан файл " + utilsJava + ".");
-                sb.append("Создан файл " + utilsJava + ". \n");
+                sb.append("Создан файл " + utilsJava + ".\n");
             } catch (IOException ex) {
                 System.err.println("Не удалось создать файл " + utilsJava + ".");
             }
         } else {
             System.out.println("Файл " + utilsJava + " уже существует.");
-            sb.append("Файл " + utilsJava + " уже существует \n");
+            sb.append("Файл " + utilsJava + " уже существует.\n");
         }
 
-        File dirGamesTempNewFile = new File("F:\\Programms\\IDEA-projects\\homeprojects\\files\\files-installed\\Games\\temp");
+        File dirGamesTempNewFile = new File("F:\\Programms\\IDEA-projects\\homeprojects\\files\\files-installed\\Games\\temp\\");
 
         File tempTxt = new File(dirGamesTempNewFile, "temp.txt");
 
         if (!tempTxt.exists()) {
             try {
                 if (tempTxt.createNewFile())
-                    System.out.println("Создан файл " + tempTxt + ". \n");
+                    System.out.println("Создан файл " + tempTxt + ".\n");
                 sb.append("Создан файл " + tempTxt + ".");
             } catch (IOException ex) {
                 System.err.println("Не удалось создать файл " + tempTxt + ".");
             }
         } else {
             System.out.println("Файл " + tempTxt + " уже существует.");
-            sb.append("Файл " + tempTxt + " уже существует \n");
+            sb.append("Файл " + tempTxt + " уже существует.\n");
         }
 
 
